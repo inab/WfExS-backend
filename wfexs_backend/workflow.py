f7387e76809f58bec1cea0735257508191dfd53d
lrodrin <lrodrin@gmail.com>
2020-10-26 15:49:42 +0100

    refactor reformat code in workflow class

--- wfexs_backend/workflow.py
+++ wfexs_backend/workflow.py
@@ -37,36 +37,37 @@ if platform.system() == "Darwin":
 
 from collections import namedtuple
 
-MaterializedContent = namedtuple('MaterializedContent',['local','uri','prettyFilename'])
-MaterializedInput = namedtuple('MaterializedInput',['name','values'])
-
+MaterializedContent = namedtuple('MaterializedContent', ['local', 'uri', 'prettyFilename'])
+MaterializedInput = namedtuple('MaterializedInput', ['name', 'values'])
 
 
 class WFException(Exception):
     pass
 
 
-def fetchClassicURL(remote_file,cachedFilename,secContext=None):
+def fetchClassicURL(remote_file, cachedFilename, secContext=None):
     """
     Method to fetch contents from http, https and ftp
     """
     try:
-        if isinstance(secContext,dict):
+        if isinstance(secContext, dict):
             username = secContext.get('username')
             password = secContext.get('password')
             if username is not None:
                 if password is None:
                     password = ''
-                
+
                 # Time to set up user and password in URL
                 parsedInputURL = parse.urlparse(remote_file)
-                
-                netloc = parse.quote(username,safe='') + ':' + parse.quote(password,safe='') + '@' + parsedInputURL.hostname
+
+                netloc = parse.quote(username, safe='') + ':' + parse.quote(password,
+                                                                            safe='') + '@' + parsedInputURL.hostname
                 if parsedInputURL.port is not None:
-                    netloc += ':' + parsedInputURL.port
-                
+                    netloc += ':' + str(parsedInputURL.port)
+
                 # Now the credentials are properly set up
-                remote_file = parse.urlunparse((parsedInputURL.scheme,netloc,parsedInputURL.path,parsedInputURL.params,parsedInputURL.query,parsedInputURL.fragment))
+                remote_file = parse.urlunparse((parsedInputURL.scheme, netloc, parsedInputURL.path,
+                                                parsedInputURL.params, parsedInputURL.query, parsedInputURL.fragment))
         with request.urlopen(remote_file) as url_response, open(cachedFilename, 'wb') as download_file:
             shutil.copyfileobj(url_response, download_file)
     except Exception as e:
@@ -96,7 +97,7 @@ class WF:
 
     RECOGNIZED_TRS_DESCRIPTORS = dict(map(lambda t: (t['trs_descriptor'], t), WORKFLOW_ENGINES))
     RECOGNIZED_ROCRATE_PROG_LANG = dict(map(lambda t: (t['rocrate_programming_language'], t), WORKFLOW_ENGINES))
-    
+
     DEFAULT_SCHEME_HANDLERS = {
         'http': fetchClassicURL,
         'https': fetchClassicURL,
@@ -104,16 +105,19 @@ class WF:
     }
 
     @classmethod
-    def fromDescription(cls, workflow_config, local_config, creds_config={}):
+    def fromDescription(cls, workflow_config, local_config, creds_config=None):
         """
-        
+
         :param workflow_config: The configuration describing both the workflow
         and the inputs to use when it is being instantiated.
         :param local_config: Relevant local configuration, like the cache directory.
+        :param creds_config:
         :type workflow_config: dict
         :type local_config: dict
         :return: workflow configuration
         """
+        if creds_config is None:
+            creds_config = {}
         return cls(
             workflow_config['workflow_id'],
             workflow_config['version'],
@@ -148,13 +152,13 @@ class WF:
         :type local_config: dict
         :type creds_config: dict
         """
-        if not isinstance(local_config,dict):
+        if not isinstance(local_config, dict):
             local_config = {}
-        
-        if not isinstance(creds_config,dict):
+
+        if not isinstance(creds_config, dict):
             creds_config = {}
 
-        if not isinstance(params,dict):
+        if not isinstance(params, dict):
             params = {}
 
         self.id = str(workflow_id)
@@ -188,7 +192,7 @@ class WF:
         os.makedirs(self.cacheROCrateDir, exist_ok=True)
         self.cacheWorkflowInputsDir = os.path.join(cacheDir, 'wf-inputs')
         os.makedirs(self.cacheWorkflowInputsDir, exist_ok=True)
-        
+
         self.schemeHandlers = self.DEFAULT_SCHEME_HANDLERS.copy()
 
         self.repoURL = None
@@ -196,7 +200,7 @@ class WF:
         self.repoRelPath = None
         self.repoDir = None
         self.engineDesc = None
-        
+
         self.materializedParams = None
 
     def fetchWorkflow(self):
@@ -238,57 +242,60 @@ class WF:
 
     def setupEngine(self):
         pass
-    
-    def addSchemeHandler(self,scheme,handler):
-        if not isinstance(handler,(types.FunctionType,types.LambdaType,types.MethodType,types.BuiltinFunctionType,types.BuiltinMethodType)):
+
+    def addSchemeHandler(self, scheme, handler):
+        if not isinstance(handler, (
+                types.FunctionType, types.LambdaType, types.MethodType, types.BuiltinFunctionType,
+                types.BuiltinMethodType)):
             raise WFException('Trying to set for scheme {} a invalid handler'.format(scheme))
-        
+
         self.schemeHandlers[scheme.lower()] = handler
-    
+
     def materializeInputs(self):
-        theParams = self.fetchInputs(self.params,workflowInputs_destdir=self.cacheWorkflowInputsDir)
+        theParams = self.fetchInputs(self.params, workflowInputs_destdir=self.cacheWorkflowInputsDir)
         self.materializedParams = theParams
-    
-    def fetchInputs(self, params, workflowInputs_destdir=None,prefix=''):
+
+    def fetchInputs(self, params, workflowInputs_destdir=None, prefix=''):
         """
         Fetch the input files for the workflow execution.
         All the inputs must be URLs or CURIEs from identifiers.org.
         """
         theInputs = []
-        
+
         paramsIter = params.items() if isinstance(params, dict) else enumerate(params)
         for key, inputs in paramsIter:
             # We are here for the 
-            linearKey = prefix+key
+            linearKey = prefix + key
             if isinstance(inputs, dict):
                 inputClass = inputs.get('c-l-a-s-s')
                 if inputClass is not None:
                     if inputClass == "File":  # input files
                         remote_files = inputs['url']
                         if not isinstance(remote_files, list):  # more than one input file
-                            remote_files = [ remote_files ]
-                        
+                            remote_files = [remote_files]
+
                         remote_pairs = []
                         for remote_file in remote_files:
                             # We are sending the context name thinking in the future,
                             # as it could contain potential hints for authenticated access
                             contextName = inputs.get('security-context')
-                            matContent = self.downloadInputFile(remote_file,workflowInputs_destdir=workflowInputs_destdir,contextName=contextName)
+                            matContent = self.downloadInputFile(remote_file,
+                                                                workflowInputs_destdir=workflowInputs_destdir,
+                                                                contextName=contextName)
                             remote_pairs.append(matContent)
-                        
-                        theInputs.append(MaterializedInput(linearKey ,remote_pairs))
+
+                        theInputs.append(MaterializedInput(linearKey, remote_pairs))
                     else:
                         raise WFException('Unrecognized input class "{}"'.format(inputClass))
                 else:
                     # possible nested files
-                    theInputs.extend(self.fetchInputs(inputs,workflowInputs_destdir=workflowInputs_destdir,prefix=linearKey+'.'))
+                    theInputs.extend(
+                        self.fetchInputs(inputs, workflowInputs_destdir=workflowInputs_destdir, prefix=linearKey + '.'))
             else:
-                if not isinstance(inputs,list):
-                    inputs = [ inputs ]
-                theInputs.append(MaterializedInput(linearKey,inputs))
-        
-        print("downloaded workflow input files")
-        
+                if not isinstance(inputs, list):
+                    inputs = [inputs]
+                theInputs.append(MaterializedInput(linearKey, inputs))
+
         return theInputs
 
     def executeWorkflow(self):
@@ -557,6 +564,8 @@ class WF:
         Download remote file.
 
         :param remote_file: URL or CURIE to download remote file
+        :param contextName:
+        :param workflowInputs_destdir:
         :type remote_file: str
         """
         parsedInputURL = parse.urlparse(remote_file)
@@ -566,37 +575,38 @@ class WF:
 
         else:
             input_file = hashlib.sha1(remote_file.encode('utf-8')).hexdigest()
-            
+
             prettyFilename = parsedInputURL.path.split('/')[-1]
-            
+
             # Assure workflow inputs directory exists before the next step
             if workflowInputs_destdir is None:
                 workflowInputs_destdir = self.cacheWorkflowInputsDir
-            
+
             if not os.path.exists(workflowInputs_destdir):
                 try:
                     os.makedirs(workflowInputs_destdir)
                 except IOError:
                     errstr = "ERROR: Unable to create directory for workflow inputs {}.".format(workflowInputs_destdir)
                     raise WFException(errstr)
-            
+
             cachedFilename = os.path.join(self.cacheWorkflowInputsDir, input_file)
-            print("downloading workflow input: {} => {}".format(remote_file,cachedFilename))
+            print("downloading workflow input: {} => {}".format(remote_file, cachedFilename))
             if not os.path.exists(cachedFilename):
                 theScheme = parsedInputURL.scheme.lower()
                 schemeHandler = self.schemeHandlers.get(theScheme)
-                
+
                 if schemeHandler is None:
-                    raise WFException('No {} scheme handler for {}'.format(theScheme,remote_file))
-                
+                    raise WFException('No {} scheme handler for {}'.format(theScheme, remote_file))
+
                 # Security context is obtained here
                 secContext = None
                 if contextName is not None:
                     secContext = self.creds_config.get(contextName)
                     if secContext is None:
-                        raise WFException('No security context {} is available, needed by {}'.format(contextName,remote_file))
-                
+                        raise WFException(
+                            'No security context {} is available, needed by {}'.format(contextName, remote_file))
+
                 # Content is fetched here
-                schemeHandler(remote_file,cachedFilename,secContext=secContext)
-            
-            return MaterializedContent(cachedFilename,remote_file,prettyFilename)
+                schemeHandler(remote_file, cachedFilename, secContext=secContext)
+
+            return MaterializedContent(cachedFilename, remote_file, prettyFilename)
