4bb585ff23b3b7c4885333731949fd6d709094eb
José María Fernández <jose.m.fernandez@bsc.es>
2021-05-20 03:03:49 +0200

    Fixed handling of next aliased URL

--- wfexs_backend/cache_handler.py
+++ wfexs_backend/cache_handler.py
@@ -141,7 +141,7 @@ class SchemeHandlerCacheHandler:
                         
                         next_input_file = os.path.relpath(finalCachedFilename, hashDir)
                     else:
-                        next_input_file = hashlib.sha1(the_remote_file.encode('utf-8')).hexdigest()
+                        next_input_file = hashlib.sha1(inputKind.encode('utf-8')).hexdigest()
                     
                     if os.path.exists(uriCachedFilename):
                         os.unlink(uriCachedFilename)
