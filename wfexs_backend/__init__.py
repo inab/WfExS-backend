b1b242fe0b7d20b6b67768bdcbebba514b4a888a
José María Fernández <jose.m.fernandez@bsc.es>
2021-08-02 19:03:53 +0200

    Added links to automatically generated documentation about the different JSON Schemas which model local configuration, workflow staging and security context files.

--- wfexs_backend/__init__.py
+++ wfexs_backend/__init__.py
@@ -20,7 +20,7 @@ __copyright__ = '© 2020-2021 Barcelona Supercomputing Center (BSC), ES'
 __license__ = 'Apache 2.0'
 
 # https://www.python.org/dev/peps/pep-0396/
-__version__ = '0.4.6'
+__version__ = '0.4.7'
 
 def describeGitRepo(repo):
     """Describe the repository version.
