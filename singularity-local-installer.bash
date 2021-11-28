b96215396a78c96a9ff76a9fc1b49a24767d3379
José María Fernández <jose.m.fernandez@bsc.es>
2021-11-28 17:43:45 +0100

    Added script to install rootless singularity (in case it is not already available)

---
+++ singularity-local-installer.bash
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# These are the software versions being installed
+# in the virtual environment
+SINGULARITY_VER=3.9.1
+GO_VER=1.17.3
+
+USERAGENT="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0"
+
+# These are placeholders
+GO_OS=linux
+GO_ARCH=amd64
+
+# Getting the installation directory
+wfexsDir="$(dirname "$0")"
+case "${wfexsDir}" in
+	/*)
+		# Path is absolute
+		true
+		;;
+	*)
+		# Path is relative
+		wfexsDir="$(readlink -f "${wfexsDir}")"
+		;;
+esac
+
+downloadDir="$(mktemp -d --tmpdir wfexs_singularity_installer.XXXXXXXXXXX)"
+echo "$0: ${downloadDir} will be used to download third party dependencies, and later removed"
+
+cleanup() {
+	rm -rf "${downloadDir}"
+}
+
+trap cleanup EXIT ERR
+
+set -e
+
+# Before installing, check whether singularity is already available
+if type -a singularity >& /dev/null ; then
+	echo "Singularity $(singularity version) is already available in the system. Skipping install"
+	exit 0
+fi
+
+# First, be sure the environment is ready to be used
+"$wfexsDir"/installer.bash
+
+# Second, let's load the environment in order to install
+# singularity in the python profile
+envDir="$(python3 -c 'import sys; print(""  if sys.prefix==sys.base_prefix  else  sys.prefix)')"
+if [ -z "${envDir}" ] ; then
+	envDir="${wfexsDir}/.pyWEenv"
+
+	# Activating the python environment
+	envActivate="${envDir}/bin/activate"
+	source "${envActivate}"
+fi
+
+# Now, it is time to check singularity binaries availability
+if [ -x "${envDir}/bin/singularity" ] ; then
+	echo "Singularity $(singularity version) is already available in the environment. Skipping install"
+	exit 0
+fi
+
+# Compilation artifacts should go to the temporary download directory
+GOPATH="${downloadDir}/go"
+export GOPATH
+PATH="${GOPATH}/bin:${PATH}"
+
+# Third, check whether there is an available go compiler
+goVer="$(go version)"
+case "$goVer" in
+	"go version go1"*)
+		# Go is available
+		true
+		;;
+	*)
+		# Fetch and install go
+		GO_OS="$(python -c 'import platform; print(platform.system().lower())')"
+		GO_ARCH="$(python -c 'import platform; print(platform.machine())')"
+		case "$GO_ARCH" in
+			x86_64)
+				# Deriving the right name
+				GO_ARCH=amd64
+				;;
+		esac
+		goSoftDir="${downloadDir}/soft"
+		goBundle=go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz
+		# Fetching go
+		( cd "${downloadDir}" && curl -L -O https://dl.google.com/go/"${goBundle}" )
+		# Installing go in the temporary directory
+		mkdir -p "${goSoftDir}"
+		tar -x -z -C "${goSoftDir}" -f "${downloadDir}/${goBundle}"
+		# Removing go bundle
+		rm "${downloadDir}/${goBundle}"
+		
+		PATH="${goSoftDir}/go/bin:${PATH}"
+		;;
+esac
+
+# Fetch and compile singularity
+singularityBundle=singularity-ce-"${SINGULARITY_VER}".tar.gz
+( cd "${downloadDir}" && curl --header "${USERAGENT}" -L -O https://github.com/sylabs/singularity/releases/download/v"${SINGULARITY_VER}"/"${singularityBundle}" )
+tar -x -z -C "${downloadDir}" -f "${downloadDir}/${singularityBundle}"
+cd "${downloadDir}"/singularity-ce-${SINGULARITY_VER}
+
+# Now, the right moment to compile and install rootless singularity
+./mconfig --without-suid --prefix="${envDir}"
+make -C ./builddir
+make -C ./builddir install
