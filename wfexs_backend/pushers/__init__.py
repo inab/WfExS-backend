39b0d19777024ab2e68879ae66897f5721e73559
José María Fernández <jose.m.fernandez@bsc.es>
2022-05-15 05:29:33 +0200

    Created first "pusher" export plugin

---
+++ wfexs_backend/pushers/__init__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2020-2022 Barcelona Supercomputing Center (BSC), Spain
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import abc
+import logging
+from typing import Any, Mapping, Optional, Sequence, TYPE_CHECKING, Union
+from typing import cast
+from typing_extensions import Final
+
+from ..common import AbsPath, RelPath, ExportItem, SymbolicName
+from ..common import SecurityContextConfig, URIWithMetadata
+from ..common import MaterializedInput, MaterializedOutput
+from ..common import AnyContent
+
+if TYPE_CHECKING:
+    from ..workflow import WF
+
+class ExportPluginException(Exception):
+    pass
+
+class AbstractExportPlugin(abc.ABC):
+    """
+    Abstract class to model stateful export plugins
+    """
+    PLUGIN_NAME : SymbolicName = cast(SymbolicName, "")
+    def __init__(self, wfInstance: "WF", setup_block: Optional[SecurityContextConfig] = None):
+        import inspect
+        
+        self.logger = logging.getLogger(dict(inspect.getmembers(self))['__module__'] + '::' + self.__class__.__name__)
+        # This is used to resolve paths
+        self.wfInstance = wfInstance
+        self.refdir = wfInstance.getStagedSetup().work_dir
+        self.setup_block = setup_block  if isinstance(setup_block, dict)  else dict()
+    
+    @abc.abstractmethod
+    def push(self, items: Sequence[AnyContent], preferred_scheme: Optional[str] = None, preferred_id: Optional[str] = None) -> URIWithMetadata:
+        """
+        This is the method to be implemented by the stateful pusher
+        """
+        pass
+    
+    @classmethod
+    def PluginName(cls) -> SymbolicName:
+        return cls.PLUGIN_NAME
---
+++ wfexs_backend/pushers/cache_export.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2020-2022 Barcelona Supercomputing Center (BSC), Spain
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import os
+import shutil
+import tempfile
+from typing import Any, Mapping, Optional, Sequence, TYPE_CHECKING, Union
+from typing import cast
+from typing_extensions import Final
+import urllib.parse
+
+from ..common import AbsPath, RelPath, ExportItem, SymbolicName
+from ..common import SecurityContextConfig, URIWithMetadata
+from ..common import MaterializedInput, MaterializedOutput
+from ..common import AnyContent, MaterializedContent
+from ..common import CacheType, URIType
+
+from ..utils.contents import link_or_copy
+
+from . import AbstractExportPlugin, ExportPluginException
+
+if TYPE_CHECKING:
+    from ..workflow import WF
+
+class CacheExportPlugin(AbstractExportPlugin):
+    """
+    Class to model exporting results to WfExS-backend cache
+    """
+    PLUGIN_NAME : SymbolicName = cast(SymbolicName, "cache")
+    
+    def __init__(self, wfInstance: "WF", setup_block: Optional[SecurityContextConfig] = None):
+        super().__init__(wfInstance, setup_block)
+    
+    def push(self, items: Sequence[AnyContent], preferred_scheme: Optional[str] = None, preferred_id: Optional[str] = None) -> URIWithMetadata:
+        """
+        These contents will be included in the cache
+        """
+        if len(items) == 0:
+            raise ValueError("This plugin needs at least one element to be processed")
+        
+        if (preferred_scheme is None) or len(preferred_scheme) == 0:
+            raise ValueError("This plugin needs a scheme to generate a PID")
+            
+        if ':' in preferred_scheme:
+            raise ValueError(f"Scheme {preferred_scheme} contains a colon")
+        
+        if (preferred_id is None) or len(preferred_id) == 0:
+            raise ValueError("This plugin needs a preferred_id to generate a PID")
+        
+        # TODO
+        # Create temporary destination directory (if needed)
+        tmpdir = None
+        source = None
+        metadata = None
+        try:
+            if len(items) > 1:
+                tmpdir = tempfile.mkdtemp(dir=self.wfInstance.getStagedSetup().temp_dir, suffix='export')
+                source = tmpdir
+                
+                # Now, transfer all of them 
+                for i_item, item in enumerate(items):
+                    relitem = os.path.relpath(item.local, self.refdir)
+                    # Outside the relative directory
+                    if relitem.startswith(os.path.pardir):
+                        # This is needed to avoid collisions
+                        prefname : Optional[RelPath]
+                        if isinstance(item, MaterializedContent):
+                            prefname = item.prettyFilename
+                        else:
+                            prefname = item.preferredFilename
+                        
+                        if prefname is None:
+                            prefname = cast(RelPath, os.path.basename(item.local))
+                        relitem = str(i_item) + '_' + prefname
+                    dest = cast(AbsPath, os.path.join(tmpdir, relitem))
+                    link_or_copy(item.local, dest)
+            else:
+                source = items[0].local
+            
+            # Generated file URI injecting the preferred id an scheme
+            uri_to_fetch = cast(URIType, urllib.parse.urlunparse(
+                urllib.parse.ParseResult(
+                    scheme='file',
+                    netloc='',
+                    path=source,
+                    params='',
+                    query=urllib.parse.urlencode({
+                        'inject_as': f'{preferred_scheme}:{preferred_id}'
+                    }, doseq=True),
+                    fragment=''
+                )
+            ))
+            
+            contentKind, abs_path, metadata, licences = self.wfInstance.wfexs.cacheFetch(uri_to_fetch, CacheType.Input, offline=False, ignoreCache=True)
+        finally:
+            # Removing leftovers
+            if tmpdir is not None:
+                shutil.rmtree(tmpdir)
+        
+        return metadata[0]
