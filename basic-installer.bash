3c8af8bf4ea1c509bed765cf8e5e414af988d057
José Mª Fernández <jose.m.fernandez@bsc.es>
2023-04-12 00:06:32 +0200

    Added common function to check and install Go.
    As the installation of apptainer, singularity and gocryptfs from source
    needs a working Go compiler, a common function to check its availability
    and install a temporary, pre-compiled version is included in
    basic-installer.bash .
    
    Also, added logic to detect both platform OS and processor architecture.

--- basic-installer.bash
+++ basic-installer.bash
@@ -13,7 +13,65 @@ case "${wfexsDir}" in
 		;;
 esac
 
-set -e
+set -eu
+
+failed=
+for cmd in curl tar gzip mktemp grep ; do
+	type -a "$cmd" 2> /dev/null
+	retval=$?
+	if [ "$retval" -ne 0 ] ; then
+		failed=1
+		echo "ERROR: Command $cmd not found in PATH and needed for the installation"
+	fi
+done
+
+if [ -n "$failed" ] ; then
+	exit 1
+fi
+
+# This shared function is used by apptainer, singularity and gocryptfs
+checkInstallGO() {
+	local GO_VER="$1"
+	local GO_OS="$2"
+	local GO_ARCH="$3"
+	local downloadDir="$4"
+
+	# Compilation artifacts should go to the temporary download directory
+	GOPATH="${downloadDir}/go"
+	export GOPATH
+	
+	local doInstallGo
+	if type -a go >& /dev/null ; then
+		local goVer="$(go version)"
+		case "$goVer" in
+			"go version go1"*)
+				# Go is available
+				true
+			;;
+			*)
+				doInstallGo=1
+			;;
+		esac
+	else
+		doInstallGo=1
+	fi
+
+	if [ -n "$doInstallGo" ] ; then
+		local goSoftDir="${downloadDir}/soft"
+		local goBundle=go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz
+		# Fetching go https://go.dev/dl/go1.17.13.linux-amd64.tar.gz
+		echo Fetching GO from https://go.dev/dl/"${goBundle}"
+		( cd "${downloadDir}" && curl -L -O https://go.dev/dl/"${goBundle}" )
+		# Installing go in the temporary directory
+		mkdir -p "${goSoftDir}"
+		tar -x -z -C "${goSoftDir}" -f "${downloadDir}/${goBundle}"
+		# Removing go bundle
+		rm "${downloadDir}/${goBundle}"
+		
+		PATH="${goSoftDir}/go/bin:${GOPATH}/bin:${PATH}"
+		export PATH
+	fi
+}
 
 for cmd in python3 pip ; do
 	type -a "$cmd" 2> /dev/null
@@ -24,6 +82,7 @@ for cmd in python3 pip ; do
 	fi
 done
 
+failed=
 for lib in libmagic.so ; do
 	ldconfig -p | grep -qF "/${lib}"
 	retval=$?
@@ -72,3 +131,19 @@ if [ $# != 0 ] ; then
 	pip install -r "${wfexsDir}"/dev-requirements.txt -r "${wfexsDir}"/mypy-requirements.txt
 	"$@"
 fi
+
+declare -a platformSuffixes=(
+	$(python -c 'import platform; print("{0} {1}".format(platform.system().lower(), platform.machine()))')
+)
+platformOS="${platformSuffixes[0]}"
+platformArch="${platformSuffixes[1]}"
+platformSuffix="${platformOS}-${platformArch}"
+platformSuffixRev="${platformArch}-${platformOS}"
+
+declare -A archesGO=(
+	[x86_64]=amd64
+	[aarch64]=arm64
+)
+
+platformArchGO="${archesGO[$platformArch]:-$platformArch}"
+
