beb0f48f32f465981ef2b6914658936f04efc050
José Mª Fernández <jose.m.fernandez@bsc.es>
2023-08-24 17:06:02 +0200

    Cosmetic changes in tests so `pre-commit run -a` is happy.

--- tests/fetchers/test_git.py
+++ tests/fetchers/test_git.py
@@ -1,5 +1,18 @@
 import pytest
 import logging
+from typing import (
+    cast,
+    TYPE_CHECKING,
+)
+
+if TYPE_CHECKING:
+    from wfexs_backend.common import (
+        RelPath,
+        RepoTag,
+        RepoURL,
+        URIType,
+    )
+
 from wfexs_backend.common import RemoteRepo, RepoType
 from wfexs_backend.fetchers.git import guess_git_repo_params
 
@@ -10,91 +23,97 @@ from wfexs_backend.fetchers.git import guess_git_repo_params
         (
             "https://github.com/inab/WfExS-backend.git",
             RemoteRepo(
-                repo_url="https://github.com/inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "https://github.com/inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "git+https://github.com/inab/WfExS-backend.git",
             RemoteRepo(
-                repo_url="https://github.com/inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "https://github.com/inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "https://github.com/inab/WfExS-backend.git@0.1.2",
             RemoteRepo(
-                repo_url="https://github.com/inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "https://github.com/inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
-                tag="0.1.2",
+                tag=cast("RepoTag", "0.1.2"),
             ),
         ),
         (
             "https://github.com/inab/WfExS-backend.git#subdirectory=workflow_examples/ipc/cosifer_test1_cwl.wfex.stage",
             RemoteRepo(
-                repo_url="https://github.com/inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "https://github.com/inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
-                rel_path="workflow_examples/ipc/cosifer_test1_cwl.wfex.stage",
+                rel_path=cast(
+                    "RelPath", "workflow_examples/ipc/cosifer_test1_cwl.wfex.stage"
+                ),
             ),
         ),
         (
             "ssh://git@github.com:inab/WfExS-backend.git",
             RemoteRepo(
-                repo_url="git@github.com:inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "git@github.com:inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "git+ssh://git@github.com:inab/WfExS-backend.git",
             RemoteRepo(
-                repo_url="git@github.com:inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "git@github.com:inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "ssh://git@github.com:inab/WfExS-backend.git@0.1.2",
             RemoteRepo(
-                repo_url="git@github.com:inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "git@github.com:inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
-                tag="0.1.2",
+                tag=cast("RepoTag", "0.1.2"),
             ),
         ),
         (
             "ssh://git@github.com:inab/WfExS-backend.git#subdirectory=workflow_examples/ipc/cosifer_test1_cwl.wfex.stage",
             RemoteRepo(
-                repo_url="git@github.com:inab/WfExS-backend.git",
+                repo_url=cast("RepoURL", "git@github.com:inab/WfExS-backend.git"),
                 repo_type=RepoType.Git,
-                rel_path="workflow_examples/ipc/cosifer_test1_cwl.wfex.stage",
+                rel_path=cast(
+                    "RelPath", "workflow_examples/ipc/cosifer_test1_cwl.wfex.stage"
+                ),
             ),
         ),
         (
             "file:///inab/WfExS-backend/.git",
             RemoteRepo(
-                repo_url="file:///inab/WfExS-backend/.git",
+                repo_url=cast("RepoURL", "file:///inab/WfExS-backend/.git"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "git+file:///inab/WfExS-backend/.git",
             RemoteRepo(
-                repo_url="file:///inab/WfExS-backend/.git",
+                repo_url=cast("RepoURL", "file:///inab/WfExS-backend/.git"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "file:///inab/WfExS-backend/.git@0.1.2",
             RemoteRepo(
-                repo_url="file:///inab/WfExS-backend/.git",
+                repo_url=cast("RepoURL", "file:///inab/WfExS-backend/.git"),
                 repo_type=RepoType.Git,
-                tag="0.1.2",
+                tag=cast("RepoTag", "0.1.2"),
             ),
         ),
         (
             "file:///inab/WfExS-backend/.git#subdirectory=workflow_examples/ipc/cosifer_test1_cwl.wfex.stage",
             RemoteRepo(
-                repo_url="file:///inab/WfExS-backend/.git",
+                repo_url=cast("RepoURL", "file:///inab/WfExS-backend/.git"),
                 repo_type=RepoType.Git,
-                rel_path="workflow_examples/ipc/cosifer_test1_cwl.wfex.stage",
+                rel_path=cast(
+                    "RelPath", "workflow_examples/ipc/cosifer_test1_cwl.wfex.stage"
+                ),
             ),
         ),
         (
@@ -108,27 +127,27 @@ from wfexs_backend.fetchers.git import guess_git_repo_params
         (
             "ssh://git@github.com:inab/WfExS-backend",
             RemoteRepo(
-                repo_url="git@github.com:inab/WfExS-backend",
+                repo_url=cast("RepoURL", "git@github.com:inab/WfExS-backend"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "https://github.com/inab/WfExS-backend",
             RemoteRepo(
-                repo_url="https://github.com/inab/WfExS-backend",
+                repo_url=cast("RepoURL", "https://github.com/inab/WfExS-backend"),
                 repo_type=RepoType.Git,
             ),
         ),
         (
             "file:///inab/WfExS-backend",
             RemoteRepo(
-                repo_url="file:///inab/WfExS-backend",
+                repo_url=cast("RepoURL", "file:///inab/WfExS-backend"),
                 repo_type=RepoType.Git,
             ),
         ),
     ],
 )
-def test_guess_git_repo_params(url, expected):
+def test_guess_git_repo_params(url: "str", expected: "RemoteRepo") -> "None":
     logger = logging.Logger("name")
-    output = guess_git_repo_params(url, logger=logger)
+    output = guess_git_repo_params(cast("URIType", url), logger=logger)
     assert output == expected
