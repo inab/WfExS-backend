25778aa6031808111043666a9a3f832089535f61
José Mª Fernández <jose.m.fernandez@bsc.es>
2024-12-04 00:00:20 +0100

    Version bump to 1.0.0rc2

--- wfexs_backend/__init__.py
+++ wfexs_backend/__init__.py
@@ -21,7 +21,7 @@ __copyright__ = "© 2020-2024 Barcelona Supercomputing Center (BSC), ES"
 __license__ = "Apache 2.0"
 
 # https://www.python.org/dev/peps/pep-0396/
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0rc2"
 __url__ = "https://github.com/inab/WfExS-backend"
 __official_name__ = "WfExS-backend"
 
